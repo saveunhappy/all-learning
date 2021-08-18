6f7d8df7ae3ff6e681843ebaa1367b62ee0d4836
JiangTaoHou <925147954@qq.com>
2021-08-18 19:43:51 +0800

    init Project

---
+++ src/test/java/com/imooc/houjiangtao/alllearning/AllLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imooc.houjiangtao.alllearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllLearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
